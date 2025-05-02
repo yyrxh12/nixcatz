976c0ee455068e0fd34f158a771ca06391ec8603
Anatolii Palivoda <anatolii.palivoda@rwth-aachen.de>
2025-05-02 12:44:00 +0200

    a

--- flake.nix
+++ flake.nix
@@ -344,7 +344,7 @@
           # or, whatever you named the package definition in the packageDefinitions set.
           # WARNING: MAKE SURE THESE DONT CONFLICT WITH OTHER INSTALLED PACKAGES ON YOUR PATH
           # That would result in a failed build, as nixos and home manager modules validate for collisions on your path
-          aliases = [ "vi" "catz" ];
+          aliases = [ "catz" ];
 
           # explained below in the `regularCats` package's definition
           # OR see :help nixCats.flake.outputs.settings for all of the settings available
